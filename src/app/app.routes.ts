b41de6c1bfee7113bb1644809032206607895116
duarte.eartes <duarte.eartes@gmail.com>
2025-06-14 00:15:46 +0200

    Home terminada. Seguimos con Sobre mí

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'sobre-mi', component: SobreMiComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'contacto', component: ContactoComponent },
+    { path: '**', component: HomeComponent }
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-header',
   standalone: true,
   imports: [RouterModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
 })
-export class HomeComponent {
+export class HeaderComponent {
 
 }
