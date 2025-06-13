0e1bfc8fdde19a65160ada14218362472a3bd992
duarte.eartes <duarte.eartes@gmail.com>
2025-06-13 19:40:48 +0200

    Añadiendo fondo y tipografía

---
+++ src/app/card-trabajo/card-trabajo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-trabajo',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-trabajo.component.html',
+  styleUrl: './card-trabajo.component.css'
+})
+export class CardTrabajoComponent {
+
+}
