5ca591f4680e9b3906cc43dd00b3e917663d2865
duarte.eartes <duarte.eartes@gmail.com>
2025-06-14 16:58:44 +0200

    Sobre mí FIN

--- src/app/sobre-mi/sobre-mi.component.ts
+++ src/app/sobre-mi/sobre-mi.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mi',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './sobre-mi.component.html',
   styleUrl: './sobre-mi.component.css'
 })
-export class SobreMiComponent {
-
+export class SobreMiComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const downloadBtn = document.getElementById('downloadCV');
+    if (downloadBtn) {
+      downloadBtn.addEventListener('click', () => {
+        const link = document.createElement('a');
+        link.href = 'assets/CvSilviaDuarteLargo2025.pdf';
+        link.download = 'CvSilviaDuarteLargo2025.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    }
+  }
 }
