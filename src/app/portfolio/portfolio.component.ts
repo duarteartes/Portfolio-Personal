da14c950327d57946ddc2d137b4f26fa7a744582
duarte.eartes <duarte.eartes@gmail.com>
2025-06-14 18:45:20 +0200

    Comenzamos con el Portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardTrabajoComponent } from '../card-trabajo/card-trabajo.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CardTrabajoComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
