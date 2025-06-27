285c349d21a5da3557a7bea5ef028a393cbca34b
duarte.eartes <duarte.eartes@gmail.com>
2025-06-27 19:11:48 +0200

    portfolio y sobre mi arreglado animaciones responsive

--- api/server.js
+++ api/server.js
@@ -15,4 +15,4 @@ app.use('/api/contact', contactRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Servidor corriendo en puerto ${PORT}`));
